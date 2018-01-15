b11eaf0a67cad2405f6bf011021c89dbef13a2d9
GUENDELI Omar <guendeliomar@gmail.com>
2018-01-15 00:35:33 +0000

    Header component added

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 // AppRegistry.registerComponent('albums', () => App);
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 // write a component
 const App = () => {
     return (
-        <Text> SOME TEXT </Text>
+        <Header />
     );
 }
 
